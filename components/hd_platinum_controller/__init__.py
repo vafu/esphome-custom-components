7af781e6a5f8e7fc397f5ab041dada207fc0bc5a
Stafford Brunk <wingrunr21@gmail.com>
2024-02-25 10:54:30 -0700

    Don't default to pin 4. Make it required instead.

--- components/hd_platinum_controller/__init__.py
+++ components/hd_platinum_controller/__init__.py
@@ -1,13 +1,12 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_CS_PIN
+from esphome.const import CONF_ID, CONF_CS_PIN, CONF_IRQ_PIN
 
 DEPENDENCIES = ["spi"]
 MULTI_CONF = True
 
 CONF_CE_PIN = "ce_pin"
 CONF_REMOTE_ID = "remote_id"
-CONF_IRQ_PIN = "irq_pin"
 
 hd_platinum_controller_ns = cg.esphome_ns.namespace("hd_platinum_controller")
 HDPlatinumController = hd_platinum_controller_ns.class_(
@@ -31,7 +30,7 @@ async def to_code(config):
 
     cg.add(var.set_cs_pin(config[CONF_CS_PIN]))
     cg.add(var.set_ce_pin(config[CONF_CE_PIN]))
-    cg.add(var.set_irq_pin(config.get(CONF_IRQ_PIN, 4)))
+    cg.add(var.set_irq_pin(config[CONF_IRQ_PIN]))
     cg.add(var.set_remote_id(config[CONF_REMOTE_ID]))
 
     cg.add_library("nrf24/RF24", None)
